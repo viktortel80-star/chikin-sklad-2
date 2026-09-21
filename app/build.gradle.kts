9acf31ec71d28ce0bb415bf9305301478714fccf
viktortel80-star <viktortel80@gmail.com>
2026-09-21 15:03:53 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,14 @@ plugins {
 android {
     namespace = "com.example.chikinsklad"
     compileSdk = 35
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 
     defaultConfig {
         applicationId = "com.example.chikinsklad"
